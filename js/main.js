6288929bdd5967918937eaadbea322bea1f03769
Zhassulan <190103383@stu.sdu.edu.kz>
2022-07-25 21:03:16 +0500

    upgrade of navbar and home section

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// variables
+const navM = document.getElementById('nav_menu');
+const navT = document.getElementById('nav_toggle');
+const navC = document.getElementById('nav_close');
+const navL = document.querySelectorAll('.nav_link')
+
+// show navbar
+if (navT) {
+	navT.addEventListener('click', (e) => {
+		navM.classList.add('show_menu');
+	})
+}
+
+// hide navbar
+if (navC) {
+	navC.addEventListener('click', (e) => {
+		navM.classList.remove('show_menu');
+	});
+
+	// hides nav-menu, while clicking nav_links
+	navL.forEach(e => {
+		e.addEventListener('click', (e) => {
+			navM.classList.remove('show_menu');
+		});
+	});
+}
