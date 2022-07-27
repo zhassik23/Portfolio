9cb604b306042c299260819277b45850b5b582f1
Zhassulan <190103383@stu.sdu.edu.kz>
2022-07-27 22:08:08 +0500

    qualification section about education that I have and my achievements

--- js/main.js
+++ js/main.js
@@ -1,27 +1,71 @@
+// navbar
 // variables
 const navM = document.getElementById('nav_menu');
 const navT = document.getElementById('nav_toggle');
 const navC = document.getElementById('nav_close');
-const navL = document.querySelectorAll('.nav_link')
+const navL = document.querySelectorAll('.nav_link');
 
 // show navbar
 if (navT) {
-	navT.addEventListener('click', (e) => {
+	navT.addEventListener('click', e => {
 		navM.classList.add('show_menu');
 		navT.classList.add('hide_toggle');
-	})
+	});
 }
 
 // hide navbar
 if (navC) {
-	navC.addEventListener('click', (e) => {
+	navC.addEventListener('click', e => {
 		navM.classList.remove('show_menu');
 	});
 
 	// hides nav-menu, while clicking nav_links
-	navL.forEach(e => {
-		e.addEventListener('click', (e) => {
+	navL.forEach(element => {
+		element.addEventListener('click', e => {
 			navM.classList.remove('show_menu');
 		});
 	});
 }
+
+
+// skills
+// variables
+const content = document.getElementsByClassName('skills_content');
+const	header = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+	var currentClass = this.parentNode.className;
+	
+	// hide skills bars
+	for (var i = 0; i < content.length; i++) 
+		content[i].className = 'skills_content skills_close';
+	
+	// show skills bars
+	if (currentClass === 'skills_content skills_close') 
+		this.parentNode.className = 'skills_content skills_open';
+}
+
+header.forEach(element =>{
+	element.addEventListener('click', toggleSkills)
+});
+
+
+// qualification
+// variables
+const tabContents = document.getElementsByClassName('qualification_list');
+const tabs = document.querySelectorAll('[data-target]');
+
+tabs.forEach(tab =>{
+	tab.addEventListener('click', () =>{
+		const target = document.querySelector(tab.dataset.target);
+		// content
+		for (var i = 0; i < tabContents.length; i++) 
+			tabContents[i].className = 'qualification_list qualification_hide';
+		target.className = 'qualification_list qualification_show';
+		// tab
+		tabs.forEach(tab =>{
+			tab.classList.remove('qualification_active');
+		})
+		tab.classList.add('qualification_active');
+	})
+})
