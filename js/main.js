d863541a7fd9efc59ee086745c06765f8d86eaa7
Zhassulan <190103383@stu.sdu.edu.kz>
2022-07-28 05:18:31 +0500

    Section for projects that are made by me

--- js/main.js
+++ js/main.js
@@ -94,3 +94,18 @@ closeButtons.forEach(closeBtn => {
 		});
 	});
 });
+
+
+// swiper
+var swiper = new Swiper(".project_container", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+	  delay: 3000,
+	  disableOnInteraction: false,
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+});
