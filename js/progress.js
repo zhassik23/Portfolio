a917a1677b39ba8ea265eb75c620deebe8dafcb8
Zhassulan <190103383@stu.sdu.edu.kz>
2022-07-29 02:36:13 +0500

    some new js files

---
+++ js/progress.js
@@ -0,0 +1,8 @@
+// progress bar
+var progress = document.getElementById('progress_bar');
+var totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function() {
+	var progressHeight = (window.pageYOffset / totalHeight) * 100;
+	progress.style.height = progressHeight + "%";
+}
