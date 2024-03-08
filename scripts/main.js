b5a3453a4efe8e3a87b43408880db39079f2eb7c
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-03-08 10:48:17 +0500

    delete unnecessary js

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,6 @@ if (navT) {
 		navT.classList.add('hide_toggle');
 	});
 }
-
 // hide navbar
 if (navC) {
 	navC.addEventListener('click', e => {
@@ -27,7 +26,6 @@ if (navC) {
 	});
 }
 
-
 // home
 // typing script
 var typed = new Typed('.typing', {
@@ -35,7 +33,6 @@ var typed = new Typed('.typing', {
 	typeSpeed: 75,
 	loop: true
 });
-
 
 // experience
 // variables
@@ -61,62 +58,4 @@ closeButtons.forEach(closeBtn => {
 			item.classList.remove('item_active');
 		});
 	});
-});
-
-
-// qualification
-// variables
-const tabContents = document.getElementsByClassName('qualification_list');
-const tabs = document.querySelectorAll('[data-target]');
-
-tabs.forEach(tab => {
-	tab.addEventListener('click', () => {
-		const target = document.querySelector(tab.dataset.target);
-		// content show/hide
-		for (var i = 0; i < tabContents.length; i++) 
-			tabContents[i].className = 'qualification_list qualification_hide';
-		target.className = 'qualification_list qualification_show';
-		// tab color switch
-		tabs.forEach(tab => {
-			tab.classList.remove('qualification_active');
-		})
-		tab.classList.add('qualification_active');
-	});
-});
-
-
-// skills
-// variables
-const content = document.getElementsByClassName('skills_content');
-const	header = document.querySelectorAll('.skills_header');
-
-function toggleSkills() {
-	var currentClass = this.parentNode.className;
-	
-	// hide skills bars
-	for (var i = 0; i < content.length; i++) 
-		content[i].className = 'skills_content skills_close';
-	
-	// show skills bars
-	if (currentClass === 'skills_content skills_close') 
-		this.parentNode.className = 'skills_content skills_open';
-}
-
-header.forEach(element => {
-	element.addEventListener('click', toggleSkills);
-});
-
-
-// swiper
-var swiper = new Swiper('.projects_container', {
-	spaceBetween: 30,
-	centeredSlides: true,
-	autoplay: {
-	  delay: 3000,
-	  disableOnInteraction: false,
-	},
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
 });
