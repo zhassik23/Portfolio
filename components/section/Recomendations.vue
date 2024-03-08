bfb4bdaae50dc084bdb566ff3bcecabed57838d7
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-03-08 09:58:59 +0500

    recomnedation section

--- components/section/Recomendations.vue
+++ components/section/Recomendations.vue
@@ -1,5 +1,31 @@
 <template>
 	<Section id="recomendations" title="Recomendations" subtitle="Characteristics from my cheifs">
-		<div></div>
+		<div class="section-content vertical inline">
+			<div class="stars">
+				<i class="fa-solid fa-star icon"></i>
+				<i class="fa-solid fa-star icon"></i>
+				<i class="fa-solid fa-star icon"></i>
+				<i class="fa-solid fa-star icon"></i>
+				<i class="fa-solid fa-star icon"></i>
+			</div>
+
+			<a class="recomendation horizontal inline" download href="assets/files/Characteristics.pdf">
+				<i class="fa-solid fa-file icon"></i>
+				<div class="recomendation-line"></div>
+				<div>
+					<h4 class="recomendation-name">Mammadov E.</h4>
+					<h6 class="recomendation-place">TOO 'KasHIM' - August, 2021 / Atyrau</h6>
+				</div>
+			</a>
+
+			<a class="recomendation horizontal inline" download href="assets/files/Recommendations.pdf">
+				<i class="fa-solid fa-file icon"></i>
+				<div class="recomendation-line"></div>
+				<div>
+					<h4 class="recomendation-name">Sain S.</h4>
+					<h6 class="recomendation-place">'Athena plus' - May, 2022 / Almaty</h6>
+				</div>
+			</a>
+		</div>
 	</Section>
 </template>
