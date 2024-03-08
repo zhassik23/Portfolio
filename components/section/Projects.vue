384bb2e23211f73671a0a78a68f1149403fe47ca
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-03-08 08:58:30 +0500

    projects section

--- components/section/Projects.vue
+++ components/section/Projects.vue
@@ -1,5 +1,434 @@
 <template>
 	<Section id="projects" title="Projects" subtitle="My works">
-		<div></div>
+		<div class='carousel'>
+			<div class='carousel-inner horizontal'>
+				<!-- qazaq taxi project -->
+				<div class='project carousel-item'>
+					<a href='https://play.google.com/store/apps/details?id=com.driver.qazaqtaxi&pli=1' class='project-image'>
+						<img src='assets/images/qazaq-taxi.png' alt='Qazaq Taxi Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Qazaq Taxi Application</h3>
+						<h6 class='description'>Tech stack: Vue.js, Nuxt.js, Vuetify, SCSS, CSS</h6>
+						<p class='text'>App for taxi drivers and passengers, key feature is you can order Lady car type for more secure travel. Also I have prepared Admin, B2B websites for Qazaq Taxi.</p>
+						<a href='https://play.google.com/store/apps/details?id=com.driver.qazaqtaxi&pli=1'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- tez zhet project -->
+				<div class='project carousel-item'>
+					<a href='https://tez-zhet.netlify.app' class='project-image'>
+						<img src='assets/images/tez-zhet.png' alt='Tez Zhet Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Tez Zhet</h3>
+						<h6 class='description'>Tech stack: Nuxt.js, CSS, SCSS</h6>
+						<p class='text'>Website for ordering dishes and products using nuxt.</p>
+						<a href='https://tez-zhet.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- kagis geonomix project -->
+				<div class='project carousel-item'>
+					<a href='https://alauzo.kz/' class='project-image'>
+						<img src='assets/images/geonomix.png' alt='Kagis > Geonomix' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Kagis > Geonomix</h3>
+						<h6 class='description'>Tech stack: Vue.js, HTML, CSS, Django.py, PostgresSQL</h6>
+						<p class='text'>Multifunctional platform. Within the framework of regional geographic information systems / platforms, digital twins (object / city / territory), provision of interdepartmental public services, implementation of the functions of state bodies, interdepartmental automation, industry GIS, digital registers of state property, archives of documentation, monitoring of the activities of departments and subordinate enterprises, dispatching, situational centers and operational management centers.</p>
+						<a href='https://alauzo.kz/'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- rakhat project -->
+				<div class='project carousel-item'>
+					<a href='https://rakhat.a-lux.dev' class='project-image'>
+						<img src='assets/images/rakhat.png' alt='Rakhat Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Rakhat</h3>
+						<h6 class='description'>Tech stack: Vue.js, SCSS</h6>
+						<p class='text'>Single page website for Rakhat compang with 360 degree camera. Prepared with Vue3.</p>
+						<a href='https://rakhat.a-lux.dev'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- tahit project -->
+				<div class='project carousel-item'>
+					<a href='https://tahit.netlify.app' class='project-image'>
+						<img src='assets/images/tahit.png' alt='New Navat Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Tahit</h3>
+						<h6 class='description'>Tech stack: Vue.js, JavaScript, SCSS</h6>
+						<p class='text'>Multi page project in two languages with authorization and authentication for Profile account. In the catalog its comfortable filter for finding some items.</p>
+						<a href='https://tahit.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- yaq project -->
+				<div class='project carousel-item'>
+					<a href='https://alpenist-tovarov.netlify.app' class='project-image'>
+						<img src='assets/images/yaq.png' alt='Yaq Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>“Альпинист Товаров”</h3>
+						<h6 class='description'>Tech stack: React.js</h6>
+						<p class='text'>The Alpinist-Products online store, known as yaq, is a network of multi-brand sports stores offering equipment for outdoor activities, hiking, trekking, skyrunning, running and other sports and outdoor activities. The React framework was used to implement this site. This project has a rather complex filtering point where the user can search for goods by category, by color, by gender, by size, by type, and sort the goods as they wish.</p>
+						<a href='https://alpenist-tovarov.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- Qonys Toi project -->
+				<div class='project carousel-item'>
+					<a href='https://qonys-toi.netlify.app' class='project-image'>
+						<img src='assets/images/qonys-toi.png' alt='Qonys Toi Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Qonys Toi</h3>
+						<h6 class='description'>Tech stack: React.js</h6>
+						<p class='text'>Invitation website for event prepared with React.</p>
+						<a href='https://qonys-toi.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- rento project -->
+				<div class='project carousel-item'>
+					<a href='https://rentokz.netlify.app' class='project-image'>
+						<img src='assets/images/rento.png' alt='Rento Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Rento</h3>
+						<h6 class='description'>Tech stack: React.js, Redux, Firebase</h6>
+						<p class='text'>Startapp project written on react with own admin panel for both users and hosts, and also simple users. Online service for booking sport facilities. Website has phone number verification before registering the user and resetting password.</p>
+						<a href='https://rentokz.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- new navat project -->
+				<div class='project carousel-item'>
+					<a href='https://newnavat.kz' class='project-image'>
+						<img src='assets/images/new_navat.png' alt='New Navat Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>New Navat</h3>
+						<h6 class='description'>Tech stack: HTML, JavaScript, CSS, jQuery</h6>
+						<p class='text'>Multilingual site of the new, sensational, fifth point of Teahouse Navat, which is located in the city of Almaty. Website is unusual in that each page block is the same size, allowing for the benefits of snap scrolling.</p>
+						<a href='https://newnavat.kz'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- midas event project -->
+				<div class='project carousel-item'>
+					<a href='https://midasevent.kz' class='project-image'>
+						<img src='assets/images/midas_event.png' alt='Midas EVENT Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Midas EVENT</h3>
+						<h6 class='description'>Tech stack: HTML, JavaScript, CSS, jQuery</h6>
+						<p class='text'>A company that organizes various kinds of events. Famous for the organization of the show program in the 'NEBO' and the concert of Irina Kairatovna. Each part of the landing page has its own unusual animation.</p>
+						<a href='https://midasevent.kz'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- sxodim project -->
+				<div class='project carousel-item'>
+					<a href='https://github.com/zhassulaan/sxodim' class='project-image'>
+						<img src='assets/images/sxodim.png' alt='sxodim.kz' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Sxodim.kz</h3>
+						<h6 class='description'>Tech stack: React.js, Django.py</h6>
+						<p class='text'>My first full-stack project written in django framework of python and react framework of javascript.</p>
+						<a href='https://github.com/zhassulaan/sxodim'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- m1 service project -->
+				<div class='project carousel-item'>
+					<a href='https://m1-service.netlify.app' class='project-image'>
+						<img src='assets/images/m1-service.png' alt='M1 Service Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>M1 Service</h3>
+						<h6 class='description'>Tech stack: Nuxt.js, SCSS, TypeScript</h6>
+						<p class='text'>...</p>
+						<a href='https://m1-service.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- check market project -->
+				<div class='project carousel-item'>
+					<a href='https://github.com/zhassulaan/check-market' class='project-image'>
+						<img src='assets/images/check_market.png' alt='Check Market Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>“ЧЕК МАРКЕТ”</h3>
+						<h6 class='description'>Tech stack: Next.js</h6>
+						<p class='text'>Multi-page site written in next.js. The hardest part of this site is the large number of modals and forms. This company is engaged in consultations, design, sale of equipment, installation and adjustment of security systems, warranty and post-warranty service, technical support.</p>
+						<a href='https://github.com/zhassulaan/check-market'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- art - galery project -->
+				<div class='project carousel-item'>
+					<a href='https://art-galery.netlify.app' class='project-image'>
+						<img src='assets/images/art-galery.png' alt='Art - Galery Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Art - Galery</h3>
+						<h6 class='description'>Tech stack: Reacct.js, SCSS</h6>
+						<p class='text'>Website with products that can be searched. You can add product to favourites.</p>
+						<a href='https://art-galery.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- unistory project -->
+				<div class='project carousel-item'>
+					<a href='https://unistory.netlify.app' class='project-image'>
+						<img src='assets/images/unistory.png' alt='UniStory Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Unistory</h3>
+						<h6 class='description'>Tech stack: React.ts, CSS, Typescript</h6>
+						<p class='text'>Application on React, with connecting a crypto wallet and displaying information from the backend.</p>
+						<a href='https://unistory.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- torgsoft project -->
+				<div class='project carousel-item'>
+					<a href='https://torgsoft.netlify.app' class='project-image'>
+						<img src='assets/images/torgsoft.png' alt='Торг Софт Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Torg Soft</h3>
+						<h6 class='description'>Tech stack: HTML, JavaScript, CSS</h6>
+						<p class='text'>The site of the Torgsoft company, which is suitable for accounting for both retail and wholesale trade. The program specializes in automating the following areas: clothing and footwear stores; building and household materials stores; children's shops; stationery stores; salons of curtains, fabrics and accessories; enterprises for the production of various products; online stores and warehouses.</p>
+						<a href='https://torgsoft.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- young's store project -->
+				<div class='project carousel-item'>
+					<a href='https://youngsstore.netlify.app' class='project-image'>
+						<img src='assets/images/youngs_store.png' alt='Store Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Korean Clothes Store</h3>
+						<h6 class='description'>Tech stack: HTML, CSS, JavaScript, Bootstrap, jQuery</h6>
+						<p class='text'>Personal website for a shop that sells stylish Korean clothes. Here you can view products, place an order and apply for a job. It's fully responsive, so you can use it on any device.</p>
+						<a href='https://youngsstore.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- meune deutsche stadt project -->
+				<div class='project carousel-item'>
+					<a href='https://md-stadt.netlify.app' class='project-image'>
+						<img src='assets/images/md_stadt.png' alt='New Navat Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>MD Stadt</h3>
+						<h6 class='description'>Tech stack: Nuxt.js, Vue, SCSS, CSS, Typescript</h6>
+						<p class='text'>Description: App for tourists of Kazakhstan with beautiful places in our cities. Written in Nuxt 3.</p>
+						<a href='https://md-stadt.netlify.app'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- age counter project -->
+				<div class='project carousel-item'>
+					<a href='https://zhassulaan.github.io/AgeCounter' class='project-image'>
+						<img src='assets/images/age_counter.png' alt='Age Counter Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Age Counter</h3>
+						<h6 class='description'>Tech stack: JavaScript, CSS, HTML, moment.js</h6>
+						<p class='text'>An application that calculates how many years, months, days, hours... you have lived since you were born. Now you can calculate your age in more detail.</p>
+						<a href='https://zhassulaan.github.io/AgeCounter'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- iphone's calculator project -->
+				<div class='project carousel-item'>
+					<a href='https://zhassulaan.github.io/Calculator' class='project-image'>
+						<img src='assets/images/calculator.png' alt='iPhone’s Calculator Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>iPhone’s Calculator</h3>
+							<h6 class='description'>Tech stack: JavaScript, HTML, CSS</h6>
+							<p class='text'>Simple Calculator. It's designed as our modern iPhones calculator. Here you can solve problems of varying complexity. It reduces the number of mistakes, so you could use it for your examination.</p>
+							<a href='https://zhassulaan.github.io/Calculator'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- number speller project -->
+				<div class='project carousel-item'>
+					<a href='https://zhassulaan.github.io/Number-speller' class='project-image'>
+						<img src='assets/images/number_speller.png' alt='Number speller' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Number speller</h3>
+						<h6 class='description'>Tech stack: JavaScript, HTML, CSS</h6>
+						<p class='text'>Number to word counter converter in three languages.</p>
+						<a href='https://zhassulaan.github.io/Number-speller'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- coffee boom project -->
+				<div class='project carousel-item'>
+					<a href='https://zhassulaan.github.io/CoffeeBoom' class='project-image'>
+						<img src='assets/images/coffee_boom.png' alt='Coffee Boom Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Coffee Boom</h3>
+						<h6 class='description'>Tech stack: JavaScript, jQuery, HTML, CSS</h6>
+						<p class='text'>Webpage for coffee court. It's a simple single page website. You can switch pages by tags. Here it uses some animations to make a smooth transition.</p>
+						<a href='https://zhassulaan.github.io/CoffeeBoom'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+
+				<!-- portfolio project -->
+				<div class='project carousel-item'>
+					<a href='https://zhassulaan.github.io/Portfolio' class='project-image'>
+						<img src='assets/images/portfolio.png' alt='Portfolio Project' class='image' />
+					</a>
+
+					<div class='project-data vertical'>
+						<h3>Portfolio Website</h3>
+						<h6 class='description'>Tech stack: HTML, CSS, JavaScript, jQuery</h6>
+						<p class='text'>Portfolio web-page that introduces me. This project is made to show my abilities and information, such as education, skills and work experience. Thanks to this page, you don't have to download a PDF file and read boring text on one text file.</p>
+						<a href='https://zhassulaan.github.io/Portfolio'>
+							<Button text="Watch this project" icon="fa-solid fa-angles-right" />
+						</a>
+					</div>
+				</div>
+			</div>
+			<i id="prev"  class="fa-regular fa-circle-left icon carousel-button" @click="slider().prevSlide"></i>
+			<i id="next"  class="fa-regular fa-circle-right icon carousel-button" @click="slider().nextSlide"></i>
+		</div>
 	</Section>
 </template>
+
+<script setup>
+const active = ref(0);
+let timer = null;
+
+function slider() {
+	const container = document.querySelector(".carousel-inner");
+	
+	if (!container) {
+		console.error("Container element not found");
+		return { prevSlide: () => {}, nextSlide: () => {} };
+	}
+
+	const width = container.offsetWidth;
+	const length = document.querySelectorAll(".carousel-item").length;
+	const maxIndex = length - 1;
+
+	function updateTransform() {
+		const newPosition = active.value * width;
+		container.style.transform = `translateX(-${newPosition}px)`;
+		startInterval();
+	}
+	function prevSlide() {
+		clearInterval(timer);
+		if (active.value > 0)
+			active.value--;
+		else
+			active.value = maxIndex;
+		updateTransform();
+	}
+	function nextSlide() {
+		clearInterval(timer);
+		if (active.value < maxIndex)
+			active.value++;
+		else
+			active.value = 0;
+		updateTransform();
+	}
+
+	return { prevSlide, nextSlide };
+}
+function startInterval() {
+	timer = setInterval(() => {
+		slider().nextSlide();
+	}, 3000);
+}
+
+onMounted(() => {
+	startInterval();
+});
+onUnmounted(() => {
+	if (timer) {
+		clearInterval(timer);
+		timer = null;
+	}
+});
+</script>
